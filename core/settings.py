ec4c8115d4ea55cefbad75775ed6774cd51e1677
reyhanebnyd <bonyadiho@gmail.com>
2024-10-30 14:31:00 +0330

    permissions and password

--- core/settings.py
+++ core/settings.py
@@ -195,8 +195,11 @@ CHANNEL_LAYERS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "/django_static/"
-STATIC_ROOT = BASE_DIR / "django_static"
+# STATIC_URL = "/django_static/"
+# STATIC_ROOT = BASE_DIR / "django_static"
+STATIC_URL = '/static/'
+STATIC_ROOT = 'var/www/static/'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
