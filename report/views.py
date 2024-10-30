c141eb6b007c6632e615c5b2493dd1a23e17770a
Sadeghizad <mosadeghizad@gmail.com>
2024-10-31 00:52:58 +0330

    a

--- report/views.py
+++ report/views.py
@@ -2,69 +2,82 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from report.tasks import generate_admin_report
-from .models import Report,UserViewQuestion,UserViewForm,AdminReport
-from form.models import Answer,Question
+from .models import Report, UserViewQuestion, UserViewForm, AdminReport
+from form.models import Answer, Question
 from rest_framework.permissions import IsAdminUser
+
+
 class AdminReportView(APIView):
-    permission_classes =IsAdminUser
+    permission_classes = IsAdminUser
+
     def post(self, request):
-        # Trigger the Celery task to update the form stats
         generate_admin_report.delay()
 
         return Response(
             {"message": "admin report update initiated. Check logs for progress."},
-            status=status.HTTP_200_OK
+            status=status.HTTP_200_OK,
         )
-    def get(self,request):
-        return Response(AdminReport.objects.latest().report_data,status=status.HTTP_200_OK)
+
+    def get(self, request):
+        return Response(
+            AdminReport.objects.latest().report_data, status=status.HTTP_200_OK
+        )
+
+
 class UserReport(APIView):
-    def post(self,request):
-        formid=request.data.get("form_id")
+    def post(self, request):
+        formid = request.data.get("form_id")
         try:
-            last_report=Report.objects.get(form_id=formid)
+            last_report = Report.objects.get(form_id=formid)
+            print("last record exist")
         except:
-            last_report=None
-        report={}
+            last_report = None
+        report = {}
         if last_report:
-            answers = Answer.objects.filter(form_id=formid,created_at__gte=last_report.timestamp)
+            answers = Answer.objects.filter(
+                form_id=formid, created_at__gte=last_report.timestamp
+            )
             report = last_report.report
+            print("last report extracted")
         else:
             answers = Answer.objects.filter(form_id=formid)
-            report["form_id"]=formid
-            try:
-                report["views"]=UserViewForm.objects.filter(form_id=formid).count()
-            except:
-                report["views"]=0
+            report["form_id"] = formid
+            report["views"] = UserViewForm.objects.filter(form_id=formid).count()
+            report["questions"] = {}
+            questions = {}
+            print("new report ready")
 
-            report['questions']={}
-            questions={}
         for answer in answers:
-            question_id = answer.question
+            print("answer cycle start")
+            question_id = answer.question.id
+            q_idstr = str(question_id)
             option_ids = None
-            text=answer.text
+            text = answer.text
+
             if answer.option:
-                option_ids = list(answer.option.id)
+                option_ids = [answer.option.id]
             if answer.select:
                 option_ids = list(answer.select.all())
-            if not answer.text:
-                answer.text = None
-            try:    
-                questions[question_id]['views']=UserViewQuestion.objects.filter(question_id=question_id).count()
-            except:
-                questions[question_id]['views']=0
 
-            options={}
+            # Initialize the question entry if it doesn't exist
+            if q_idstr not in questions:
+                questions[q_idstr] = {"views": 0, "ans": []}  # Initialize views and ans
+
+            questions[q_idstr]["views"] = UserViewQuestion.objects.filter(
+                question_id=question_id
+            ).count()
+
+            options = {}
             for opt in option_ids:
-                opt=Question.objects.get(id=question_id).order.index(opt)
-                if options[opt]:
-                    options[opt] += 1
-                options[opt]=1
-            questions[question_id]=options
+                question = Question.objects.get(id=question_id)
+                opt_index = question.order.index(opt.id)  # Use opt.id
+                if opt_index in options:
+                    options[opt_index] += 1
+                else:
+                    options[opt_index] = 1
+
+            questions[q_idstr]["options"] = options
+
             if text:
-                if not questions[question_id]['ans']:
-                    questions[question_id]['ans']=[]
-                questions[question_id]['ans'].append(text)
-        return Response(
-            {"report": report},
-            status=status.HTTP_200_OK
-        )
+                questions[q_idstr]["ans"].append(text)  # Append answer
+        return Response({"report": report}, status=status.HTTP_200_OK)
